a6761fbd4f3512888743f07b55e54100ebd96314
msteele96 <marcsteele2014@gmail.com>
2020-12-07 20:30:45 -0500

    Scrapes each location at startup, no times

--- lib/chunky_movies_manch/cli.rb
+++ lib/chunky_movies_manch/cli.rb
@@ -1,26 +1,65 @@
 class CLI
 
     def showings
-        xyz = Scraper.new
-        # put a loop in here somewhere to be able to look at multiple movies if the user wants
-        # runs scraper to get movies, stores as local variable (array of hashes)
-        # user enters the number of the movie they would like to see
-        input = "list"
+        manchester = Scraper.new.today("manchester")
+        nashua = Scraper.new.today("nashua")
+        pelham = Scraper.new.today("pelham")
+        current_location = ""
+        input = "locations"
         until input.downcase == "exit"
-            if input.to_i > 0 && input.to_i <= xyz.today.length
-                puts "#{xyz.today[input.to_i-1][:synopsis]}"
+            loc_input = true
+            if input.to_i > 0 && input.to_i <= current_location.length
+                puts "#{current_location[input.to_i-1][:synopsis]}"
+            elsif input == "locations"
+                @location = location
+                input = "list"
+                case @location
+                when "Manchester"
+                    current_location = manchester
+                when "Nashua"
+                    current_location = nashua
+                when "Pelham"
+                    current_location = pelham
+                when "Exit"
+                    input = "exit"
+                end
+                loc_input = false
             elsif input == "list"
-                puts "Today's showings are:"
-                puts "1. #{xyz.today[0][:name]} Rated: #{xyz.today[0][:rating]} Showtimes: #{xyz.today[0][:showtimes]}"
-                puts "2. #{xyz.today[1][:name]} Rated: #{xyz.today[1][:rating]} Showtimes: #{xyz.today[1][:showtimes]}"
+                puts "Today's showings for the #{@location} location are:"
+                i = 0
+                current_location.length.times do
+                    puts "#{i+1}. #{current_location[i][:title]}, #{current_location[i][:rating]}, Showtimes: #{current_location[i][:showtimes]}"
+                    i+=1
+                end
+                # fix showtime button text
                 puts "What movie would you like to read the synopsis of?"
-                puts "Choose a number above, list to see the options again, or type exit to leave."
+                puts "Choose a number above, list to see the options again, locations to choose a new location, or exit to leave."
             else    
                 puts "I'm not sure what you would like. Type the number of a movie, list to see the options again, or exit to leave the program."
             end
-        input = gets.strip
+            if loc_input == true
+                input = gets.strip
+            end
         end
         puts "Enjoy your night at the movies!"
+    end
+
+    def location
+        puts "What Chunky's location are you going to? (Manchester, Nashua, Pelham)"
+        theatre = gets.strip.capitalize
+        case theatre
+        when "Nashua"
+            return theatre
+        when "Pelham"
+            return theatre
+        when "Manchester"
+            return theatre
+        when "Exit"
+            return theatre
+        else
+            puts "Enter a valid location."
+            location
+        end
     end
 
 end
--- lib/chunky_movies_manch/scraper.rb
+++ lib/chunky_movies_manch/scraper.rb
@@ -1,15 +1,22 @@
 class Scraper
 
-    attr_accessor :name, :showings, :rating, :synopsis
+    attr_accessor :movie_ary, :title, :showtimes, :rating, :synopsis
 
-    def today
-        @stubbed = [{name: "movie_1", rating: "PG", showtimes: "7:30", synopsis: "This is a family movie"}, {name: "movie_2", rating: "R", showtimes: "9:00", synopsis: "For adult audiences only"}]
-        return @stubbed
-    end
-
-    def scrape
-        doc = Nokogiri::HTML(open("https://www.chunkys.com/theatre/manchester/now-showing/"))
-        binding.pry
+    def today(site)
+        doc = Nokogiri::HTML(open("https://www.chunkys.com/theatre/#{site}/now-showing/"))
+        movie_cards = doc.search("div.col-xs-12.film-row")
+        movie_info = movie_cards.search("header.entry-header")
+        @movie_ary = []
+            movie_info.each do |info|
+                film = {}
+                film[:rating] = info.search("h5").search("div").text.strip
+                film[:title] = info.search("h5").search("a").children[0].text.strip
+                film[:synopsis] = info.search("div.row.text-left").text.gsub("... READ MORE","")
+                # film[:showtimes] = info.search("a.btn")
+                film[:showtimes] = "time"
+                @movie_ary << film
+            end
+        return @movie_ary
     end
 
 end
